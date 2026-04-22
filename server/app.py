487df22c788cc5ab9d6fc9fb2c7d1dd8fb28ce4c
kashik09 <kashiku789@gmail.com>
2026-04-22 11:47:24 +0300

    Add login/logout/check_session auth routes

--- server/app.py
+++ server/app.py
@@ -20,12 +20,51 @@ api = Api(app)
 
 class ClearSession(Resource):
 
-    def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
+    def _clear(self):
+        session.pop('page_views', None)
+        session.pop('user_id', None)
+        return '', 204
 
-        return {}, 204
+    def get(self):
+        return self._clear()
+
+    def delete(self):
+        return self._clear()
+
+
+class Login(Resource):
+
+    def post(self):
+        username = request.get_json().get('username')
+        user = User.query.filter_by(username=username).first()
+
+        if not user:
+            return {}, 401
+
+        session['user_id'] = user.id
+        return UserSchema().dump(user), 200
+
+
+class Logout(Resource):
+
+    def delete(self):
+        session.pop('user_id', None)
+        return '', 204
+
+
+class CheckSession(Resource):
+
+    def get(self):
+        user_id = session.get('user_id')
+
+        if not user_id:
+            return {}, 401
+
+        user = User.query.filter(User.id == user_id).first()
+        if not user:
+            return {}, 401
+
+        return UserSchema().dump(user), 200
 
 class IndexArticle(Resource):
     
@@ -51,6 +90,9 @@ class ShowArticle(Resource):
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
